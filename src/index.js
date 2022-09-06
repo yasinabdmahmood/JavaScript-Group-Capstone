0237b7aaba6941e15bfcb6dc9b6cca0dc9b4b3e8
Degisew Mengist <you@example.com>
2022-09-06 18:24:53 +0300

    webpack setup added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './SCSS/style.scss';
+import Icon from './asset/images/app_screenshoot.png';
+
+
+const myIcon = new Image();
+myIcon.src = Icon;
+const element = document.body;
+element.appendChild(myIcon);
