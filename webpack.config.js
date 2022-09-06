18c03954500dbd2d2c7b72fe90892d96f5648440
Degisew Mengist <you@example.com>
2022-09-06 18:27:20 +0300

    Config file added.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Javascript capstone",
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
