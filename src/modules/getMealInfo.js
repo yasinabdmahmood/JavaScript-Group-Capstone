7bdbe3f6db1a05fcae4ae26c667da7ba4dfd2ae4
yasinabdmahmood <abd181103@gmail.com>
2022-09-07 09:15:13 +0300

    Select TheMealDB API to be used in the project

---
+++ src/modules/getMealInfo.js
@@ -0,0 +1,8 @@
+import { idList } from "./db.js";
+const getMealInfo = async (id) => {
+    const response = await fetch(idList+id);
+    const {meals} = await response.json();
+    return meals[0];
+    };
+
+    export default getMealInfo
