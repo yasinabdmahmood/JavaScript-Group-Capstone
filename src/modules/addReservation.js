d0c366991df1ac319c2afb6cab36215d424a9aa5
Dadadon <davianberoni@yahoo.com>
2022-09-09 09:50:03 -0400

    update files

---
+++ src/modules/addReservation.js
@@ -0,0 +1,14 @@
+const reserve = async (url, data) => {
+  const post = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+    await fetch(url, post);
+    
+  console.log(url);
+};
+
+export default reserve;
--- src/modules/displayReservation.js
+++ src/modules/displayReservation.js
@@ -1,6 +1,8 @@
+import fetchReservations from "./fetchReservation";
+const apiUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/tGV16CMoj3AJeC9Xh2Mu/reservations";
 const displayReservation = (e) => {
   const { id } = e.target.parentNode.querySelector('.comment-button');
-
+  fetchReservations(apiUrl, id);
   console.log(id);
 };
 
